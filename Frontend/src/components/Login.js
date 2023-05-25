19fa19084d7efbaf6edc9bfdda116fc125c64567
shazm12 <shamik.bera@getfundwave.com>
2023-05-26 00:39:24 +0530

    form data valiudation and error handling for api calls

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -4,28 +4,44 @@ import { BASE_URL } from '../constants'
 
 const Login = (props) => {
     const [credentials, setCredentials] = useState({ email: "", password: "" });
+    const [loading, setLoading ] = useState(false);
     let navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch(`${BASE_URL}/api/auth/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: credentials.email, password: credentials.password })
-        });
-        const json = await response.json();
-        console.log(json);
-        if (json.success) {
-            // setting the token value to token variable
-            localStorage.setItem('token', json.authtoken);
-            navigate('/');
-
-        } else {
-            alert("Invalid credentials");
+        if(credentials.email === "" || credentials.password ===  "") {
+            alert("Please enter your credentials!");
+            return;
         }
+
+        setLoading(true);
+            
+        try {
+            const response = await fetch(`${BASE_URL}/api/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: credentials.email, password: credentials.password })
+            });
+            const json = await response.json();
+            console.log(json);
+            if (json.success) {
+                // setting the token value to token variable
+                localStorage.setItem('token', json.authtoken);
+                navigate('/');
+
+            } else {
+                alert("Invalid credentials");
+            }
+        
+        }
+         catch(err) {
+            console.error(err);
+            alert(err);
+        }
+        setLoading(false);
     }
 
     const onChange = (e) => {
@@ -48,7 +64,7 @@ const Login = (props) => {
                     <input type="password" className="form-control" name="password" value={credentials.password} onChange={onChange} id="password" />
                 </div>
 
-                <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+                <button type="submit" className="btn btn-primary" disabled={loading} onClick={handleSubmit}>{!loading ? "Submit" : "Submitting"}</button>
             </form>
         </div>
     )
